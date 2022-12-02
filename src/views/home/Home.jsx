dd14a67cc0af1d5d4012870f05a161bbc55f172f
Camilo Rocca <camilorocca@gmail.com>
2022-12-02 10:15:11 +0100

    Clean home from redundant imports

--- src/views/home/Home.jsx
+++ src/views/home/Home.jsx
@@ -1,26 +1,13 @@
 import React from "react";
-import { useState } from "react";
-
-import NavBar from "../../components/navBar/NavBar";
-import SearchBar from "../../components/searchBar/SearchBar";
-import CategoryBox from "../../components/categoryBox/CategoryBox";
-import Button from "../../components/button/Button.jsx";
-import TextFieldOneLine from "../../components/textFieldOneLine/TextFieldOneLine.jsx";
+import LoadApp from "../../components/loadApp/LoadApp";
+import DropDown from "../../components/dropDown/DropDown";
 
 const Home = () => {
-  const [navOrSearch, setNavOrSearch] = useState(false);
   return (
     <React.Fragment>
-      {navOrSearch === true ? (
-        <NavBar navOrSearch={navOrSearch} setNavOrSearch={setNavOrSearch} />
-      ) : (
-        <SearchBar navOrSearch={navOrSearch} setNavOrSearch={setNavOrSearch} />
-      )}
-      <CategoryBox />
-      <Button />
-      <TextFieldOneLine />
+      <LoadApp />
+      <DropDown />
     </React.Fragment>
-  );
-};
+  );};
 
 export default Home;
