58f9f0bfc8ac05a4c047795c5a9f46bab2eedebb
Gary HL <103480668+GaryHL@users.noreply.github.com>
2022-12-02 07:02:36 +0100

    basic filter

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,16 +1,30 @@
-import React from "react";
+import React, {useState} from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from '../views/home/Home';
 import Filter from "../views/filter/Filter";
 
 function Router() {
+
+      const [settignsFilter, setSettingsFilter] = useState({
+         "web":``,
+         "ios":``,
+         "android": ``,
+         "productivity": ``,
+         "lifeStyle" : ``,
+         "social" : ``,
+         "photography" : ``,
+         "tools" : ``,
+         "entertainment" : ``,
+         "videoEditor" : ``,
+      })
+
    return (
       <BrowserRouter>
          <Routes>
             <Route path="/" element={<Home/>} />
             <Route path="/home" element={<Home/>} />
-            <Route path="/filter" element={<Filter/>} />
+            <Route path="/filter" element={<Filter setSettingsFilter={setSettingsFilter} settignsFilter={settignsFilter}/>} />
          </Routes>
       </BrowserRouter>
    );
