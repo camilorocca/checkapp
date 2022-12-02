f5d1e92e6a428b05f384280cc424ce95daa1cfb0
Camilo Rocca <camilorocca@gmail.com>
2022-12-02 12:15:20 +0100

    create new AppNameInput

---
+++ src/components/appNameInput/AppNameInput.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { TextField } from "./AppNameInput_styled";
+
+const AppNameInput = () => {
+  return (
+    <React.Fragment>
+      <TextField placeholder="Text text text" />
+    </React.Fragment>
+  );
+};
+
+export default AppNameInput;
