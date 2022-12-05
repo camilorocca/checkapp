3c9a2dd4515fc136749d71bfed5e0ec267dea27a
Camilo Rocca <camilorocca@gmail.com>
2022-12-05 09:10:28 +0100

    create new view with appUploadForm

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -3,7 +3,14 @@ import { MainButton } from "./Button_styled";
 import { AiOutlineArrowRight } from "react-icons/ai";
 
 const Button = () => {
-  return <MainButton>Longer Text<AiOutlineArrowRight/></MainButton>;
+  return (
+    <React.Fragment>
+      <MainButton>
+        Longer Text
+        <AiOutlineArrowRight />
+      </MainButton>
+    </React.Fragment>
+  );
 };
 
 export default Button;
